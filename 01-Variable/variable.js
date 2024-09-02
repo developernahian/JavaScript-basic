de588c96118fadbb6f73457e9080a87d948d386b
developernahian <developer.nahian@gmail.com>
2024-09-02 13:12:46 +0600

    variable

---
+++ 01-Variable/variable.js
@@ -0,0 +1,48 @@
+/*
+VARIABLE
+5 things to declare a variable
+var weight = 50 ;
+*/
+
+/*
+var let const
+we should use only let and const
+var old way
+*/
+
+
+
+/*
+naming Conventon
+
+1. no deyword in variable
+2. no space or gap in variable name
+3. no quote
+4. can not stat with a number. but number other than the first letter is alowed
+5. name is case sensitive
+6.prefer camel case
+
+*/
+
+
+/*
+my_current_home_address  (snake case)
+myCurrentHomeAddress  (camel case)
+MyCurrentHomeAddress  (Pascal case)
+*/
+
+var weight = 50;
+console.log(weight);
+
+var price = 100;
+price = 200;
+console.log(price);
+
+var height;
+console.log(height);
+
+
+let a = 10;
+const b = 20;
+
+
